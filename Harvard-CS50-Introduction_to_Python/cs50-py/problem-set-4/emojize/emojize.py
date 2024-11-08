3136af780ff6e8a4c49c90dad50ac87d2290633f
Erre030 <118534851+Erre030@users.noreply.github.com>
2024-11-08 21:13:33 +0100

    Add files via upload

---
+++ Harvard-CS50-Introduction_to_Python/cs50-py/problem-set-4/emojize/emojize.py
@@ -0,0 +1,28 @@
+#pip install emoji
+
+import emoji
+
+def main():
+    out_var = input("Input: ")
+    print(emoji.emojize(out_var, language='alias'))     # supports full list and aliases
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
