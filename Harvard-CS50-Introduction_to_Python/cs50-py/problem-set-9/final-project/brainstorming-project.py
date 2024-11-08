0988d6e7638845ea14599765ba8a11c78f46b059
Erre030 <118534851+Erre030@users.noreply.github.com>
2024-11-08 21:09:10 +0100

    Add files via upload

---
+++ Harvard-CS50-Introduction_to_Python/cs50-py/problem-set-9/final-project/brainstorming-project.py
@@ -0,0 +1,33 @@
+#project ideas:
+    #1.task tracker application
+    #2.expense tracker
+    #3.personal health tracker
+
+
+#expense tracker:
+
+    #functionality/functions:
+
+        #add_expense(amount, category, description, regularity of payment) --> adds expense (maybe use specific name or class to easily add/change/delete expenses)
+            #--> e.g. 30$, food, weekly purchase, weekly
+
+        #rm expense(...)    --> removes expense
+
+
+        #summary_category(category-name):
+            #summarizes expenses by category
+
+        #summary_recurring(category-name):
+            #summarizes expenses by regularity of payment
+
+        #list_expenses():
+            #list all expenses cretated
+
+        #visualization():
+            #generate chart of expenses
+
+
+    #extensions:
+        #export stats via csv
+        #budget warning system
+        #compare monthly expenses with each other (which specific payment was lower maybe?)
---
+++ Harvard-CS50-Introduction_to_Python/cs50-py/problem-set-9/final-project/notes-final-project.py
@@ -0,0 +1,42 @@
+#requirements for final project:
+
+    #main function and three or more additional functions --> done
+    #three functions must be accompanied by tests --> done
+    #main function and additional functions must be in file called "project.py" which is in the root-folder of the project --> done
+    #tests in file called test_project.py which must be in root-folder of project --> done
+    #every pip-installable libraries must be listed, one per line, in a file called "requirements.txt" inside the root-folder --> none
+
+
+    #you can add additional classes or functions
+    #implementing the project should entail more time and effort as is required by each of the problem sets
+
+#---------------------------------
+
+#questions to ask yourself:
+
+    #1.What will your software do? What features will it have? How will it be executed?
+    #2.What new skills will you need to acquire? What topics will you need to research?
+    #3.If working with one or two classmates, who will do what?
+    #4.In the world of software, most everything takes longer to implement than you expect.
+    #And so it’s not uncommon to accomplish less in a fixed amount of time than you hope.
+    # What might you consider to be a good outcome for your project? A better outcome? The best outcome?
+
+#-----------------------------------
+
+#Steps to complete for the project:
+
+#Step 1 :
+    #create a short video (max. 3 min.) where you present your project
+        #must begin with opening section that displays: project title, name, github and edx usernames, city and country, date of video
+        #after opening section: show project with slides, screenshot, voice over, live action ,etc.
+        #--> help to create video in course
+
+        #load video to a video platform and copy its URL
+        #submit a form for the final project (in course)
+
+#Step 2 :
+    #Create a README.md in your "~/project" folder that explains your project
+        #file should include: project title, URL, description of project (template in course)
+        #-->further guidelines and requirements found in course
+
+#Check graded project and receive certificate/certificates (course)
